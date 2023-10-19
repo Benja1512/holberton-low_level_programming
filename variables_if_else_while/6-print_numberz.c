874c41b30d455c5a0041beec7da6a2e1f5419e61
benjamin ontiveros <bontiveros15@gmail.com>
2023-10-18 17:04:16 -0700

    5-print_numbers

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <sdtlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - all singlres numbers of base 10 from 0
+ * Description: print all the single numbers of base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		putchar('n');
+	}
+
+	putchar('\n');
+	return (0);
+}
