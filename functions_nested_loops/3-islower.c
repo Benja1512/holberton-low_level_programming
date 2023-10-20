5a462acb4f617d42204ab3c678cc41fbb2ffa0af
benjamin ontiveros <bontiveros15@gmail.com>
2023-10-20 15:58:42 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks when characters is lowercase
+ * @c: the characlowercase character
+ * Return: 0
+ */
+
+int _islower(int c)
+
+{
+
+	if (c >= 'a' && c <= 'a')
+	{
+		return (1);
+	}
+	else
+		_putchar('\n');
+		return (0);
+}
