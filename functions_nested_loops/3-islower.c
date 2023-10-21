eae42cf4a0c40ddc9c225f5285ae395e52e552b5
benjamin ontiveros <bontiveros15@gmail.com>
2023-10-21 16:45:13 -0700

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,18 +2,14 @@
 /**
  * _islower - checks when characters is lowercase
  * @c: the characlowercase character
- * Return: 0
+ * Return: 1 when character is lowercase, 0 otherwise
  */
 
 int _islower(int c)
 
 {
-
 	if (c >= 'a' && c <= 'a')
-	{
 		return (1);
-	}
 	else
-		_putchar('\n');
 		return (0);
 }
