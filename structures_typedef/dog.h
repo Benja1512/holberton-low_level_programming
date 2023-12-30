a497589f3bc47f549271ec248f8395ecf2083416
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-30 15:49:58 -0800

    fixed: fixed code

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,23 +1,26 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef dog_h
+#define dog_h
+
 /**
- * struct dog - dog data
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner of the dog
- *
- * Description: Defines metadata about the dog and type of dog's struct
+ * struct dog - Realizar definiciones de tipo
+ * @name : definicion nombre
+ * @owner : definicion del dueño
+ * @age : definicion de la edad
+ * Description: Definicion de los tipos de struct
+ * Return:
  */
+
 typedef struct dog
 {
 	char *name;
-	float age;
 	char *owner;
-} 
-dog_t;
-	int _putchar(char c);
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
+	float age;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
