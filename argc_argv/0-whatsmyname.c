956f97aef9bfc1fe9928626c8386238647a5c12d
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-30 14:35:00 -0800

    fixed: ok cide

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints a program name 
- * @argc: the amount of arguments
- * @argv: array of arguments
+ * main - int
+ * @argc: checked
+ * @argv: checked
+ * Description: Write a program that prints its name
  * Return: 0
  */
 
-int main( int __attribute__((__unused__)) argc, char * argv[])
-{
-	
+int main(int argc __attribute__((unused)), char *argv[])
+	{
 	printf("%s\n", argv[0]);
 	return (0);
-}
+	}
