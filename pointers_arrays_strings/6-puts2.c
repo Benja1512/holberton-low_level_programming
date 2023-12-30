f09f5039f151892a772cebd5514f7e981d46423f
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-30 13:40:54 -0800

    fixed: code ok

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,28 @@
-#include "main.h"
-
 /**
- * puts2 - prints part of a string
- * @str: string to be printed
- *
- * Return: void
- **/
+ * puts2 - Escribre una funcion que impre una
+ * cadena cada dos valores
+ * @str: toma un puntero a una matriz de caracteres
+ * como argumento.
+ * Descrition: imprime una cadena cada dos valores
+ * Return: 0
+ */
+
+
+#include "main.h"
+#include <stdio.h>
+
 
 void puts2(char *str)
 {
 	int i = 0;
-	while (str[i] != '\0')
 
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
-			_putchar(str[i]);
+		{
+			putchar(str[i]);
+		}
 		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
