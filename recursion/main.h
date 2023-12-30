bcb2c6bbb4200f499553041b4d5528f7ba9a703e
benjamin ontiveros <bontiveros15@gmail.com>
2023-12-30 14:13:34 -0800

    fixed: code fixed

--- recursion/main.h
+++ recursion/main.h
@@ -1,11 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int c);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int prime);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+
 #endif
